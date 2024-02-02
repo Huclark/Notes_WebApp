9dcd616c3ce687d9674176aca28b87f552ce3c2a
Huclark <vhuclark@gmail.com>
2024-02-02 14:39:20 +0000

    Update: register routes

--- website/__init__.py
+++ website/__init__.py
@@ -11,5 +11,13 @@ def create_app():
     app = Flask(__name__)
     # create secret key
     app.config['SECRET KEY'] = 'dhskhdsskdjsfkjn'
+    
+    # register all blueprints here
+    from .views import views
+    from .auth import auth
+    
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    
     # return app
     return app
