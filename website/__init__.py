4aa6480fb2eacb5dc00225b68d3d56839a3e7882
Huclark <vhuclark@gmail.com>
2024-02-03 00:15:00 +0000

    Update: configuration

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     """Creates a flask app, initializes its secret key and
@@ -10,7 +15,10 @@ def create_app():
     # name of app
     app = Flask(__name__)
     # create secret key
-    app.config['SECRET KEY'] = 'dhskhdsskdjsfkjn'
+    app.config['SECRET_KEY'] = 'dhskhdsskdjsfkjn'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
+    # initialize the database
+    db.init_app(app)
     
     # register all blueprints here
     from .views import views
@@ -19,5 +27,16 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     
+    from .models import User, Note  # so that we run our app init first loads all our
+    # models before initialising or database
+    
+    # create the database
+    create_database(app)
     # return app
     return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+            print('Database created!')
