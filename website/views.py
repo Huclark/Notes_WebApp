31109da83f05ff483049cbf05b6a3ecb6448a29a
Huclark <vhuclark@gmail.com>
2024-02-03 10:09:56 +0000

    Update: home and delete routes

--- website/views.py
+++ website/views.py
@@ -1,10 +1,35 @@
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note, db
+import json
 
 # Blueprint is simply used to indicate that this file
 # has routes (.i.e URLs). It allows us to have routes defined
 # in multiple files
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
+@login_required
 def home():
-    return render_template("home.html")
+    if request.method == 'POST':
+        note = request.form.get('note')
+        
+        if len(note) < 1:
+            flash('Note is too short :(', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added successfully :)', 'success')
+    return render_template("home.html", user=current_user)
+
+@views.route('/delete', methods=['POST'])
+@login_required
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note and note.user_id == current_user.id:
+        db.session.delete(note)
+        db.session.commit()
+    return jsonify({})
