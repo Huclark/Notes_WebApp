db9d0dd494b9d93228fc40bfe2d8cd433f1e104f
Huclark <vhuclark@gmail.com>
2024-02-03 10:09:38 +0000

    Fix: login, sign up and logout routes

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User, db
-
+from flask_login import login_user, login_required, logout_user, current_user
 # Blueprint is simply used to indicate that this file
 # has routes (.i.e URLs). It allows us to have routes defined
 # in multiple files
@@ -17,15 +17,20 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
-            else:
-                flash('Incorrect password, try again.', category='error')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+
+            flash('Incorrect password, try again.', category='error')
         else:
             flash('There is no account registered with that email.', category='error')
-    return render_template("login.html", text="testing")
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return render_template("home.html")
+    logout_user()
+    flash('Logged out successfully! Enter your details to sign in again.', category='success')
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['POST', 'GET'])
 def sign_up():
@@ -49,10 +54,14 @@ def sign_up():
             flash('Password must have at least 7 characters.', category='error')
         else:
             # add user to database
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='pbkdf2:sha256'))
+            new_user = User(
+                email=email,
+                first_name=first_name,
+                password=generate_password_hash(password1, method='pbkdf2:sha256')
+                )
             db.session.add(new_user)
             db.session.commit()
             flash('Account created successfully!', category='success')
             return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
